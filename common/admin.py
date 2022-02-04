dc660b3c6b7b143a328ddab0dab54a95028e1436
junnng <fghd1108@gmail.com>
2022-02-04 14:46:41 +0900

    로그인수정

--- common/admin.py
+++ common/admin.py
@@ -1,34 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .forms import UserChangeForm, UserCreationForm
-from .models import User
-
-#관리자화면의 사용자변경폼과 추가폼을 새로 생성한 폼으로 적용
-class UserAdmin(BaseUserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-
-    list_display = ('email', 'stockfirm', 'is_admin')
-    list_filter = ('is_admin',)
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('stockfirm',)}),
-        ('Permissions', {'fields': ('is_admin',)}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'stockfirm', 'password1', 'password2')}
-         ),
-    )
-    search_fields = ('email',)
-    ordering = ('email',)
-    filter_horizontal = ()
-
-#장고가 기본 제공하는 그룹 사용X
-admin.site.register(User, UserAdmin)
-admin.site.unregister(Group)
 # Register your models here.
