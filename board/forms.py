498f184c1b93330c67e3850ee81a502c7ad1e7aa
kdb <kdb@mysite.com>
2022-02-04 13:39:12 +0900

    board V1.1

--- board/forms.py
+++ board/forms.py
@@ -6,6 +6,11 @@ class DiscussionForm(forms.ModelForm):
     class Meta:
         model = Discussion  # 사용할 모델
         fields = ['subject', 'content', 'image']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+            # 'image': forms.ImageField(attrs={'class': 'form-control'})
+        }
         labels = {
             'subject': '제목',
             'content': '내용',
@@ -17,6 +22,10 @@ class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
         fields = ['content', 'image']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10})
+        }
         labels = {
             'content': '답글내용',
             'image': '사진',
--- board/templatetags/board_filter.py
+++ board/templatetags/board_filter.py
@@ -1,4 +1,5 @@
 # import markdown
+import markdown
 from django import template
 from django.utils.safestring import mark_safe
 
