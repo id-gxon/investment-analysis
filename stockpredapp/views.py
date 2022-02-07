cace24d54a7782e4fae1e7ee65c953e3927ff3b5
unknown <qwer5383@naver.com>
2022-02-07 14:59:41 +0900

    시각화

--- stockpredapp/views.py
+++ stockpredapp/views.py
@@ -1,14 +1,8 @@
 # Create your views here.
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Code_name
-from keras.models import Sequential
-from keras.layers import SimpleRNN, Dense
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-
 from keras.models import Sequential
 from keras.layers import SimpleRNN, Dense
 from sklearn.preprocessing import MinMaxScaler
@@ -19,10 +13,9 @@ import math
 import numpy as np
 import FinanceDataReader as fdr
 import time
-import pandas as pd
-import pandas_datareader as pdr # 주식데이터 크롤링
 
-epochs = 10
+
+epochs = 10 # 학습반복수
 
 
 def s_index(request):
@@ -60,7 +53,7 @@ def result(request, stock_id):
     name = Code_name.objects.get(id=stock_id).stock_code
     # result = rnn(stock.stock_code+'KQ', start='2021-07-01', end='2022-01-01')
     Result = rnn(name, start='2021-01-01', end='2022-02-03')
-    result = Result[0]
+    result = Result[0]  # rnn 함수결과를 변수에 할당
     result_d1 = Result[1]
     result_d2 = Result[2]
     result_d3 = Result[3]
@@ -79,7 +72,7 @@ def result(request, stock_id):
     predPlot = Result[16]
     date = Result[17]
 
-    context = {'stock':stock,
+    context = {'stock':stock, # 분석결과 페이지에 보낼 데이터 정리
                'result': result,
                'result_d1': result_d1,
                'result_d2': result_d2,
@@ -98,26 +91,23 @@ def result(request, stock_id):
                'testPredictPlot':testPredictPlot,
                'predPlot': predPlot,
                'date' : date
-
-               }
-
+                }
     return render(request, 'stockpredapp/stock_result.html', context)
 
 
 def rnn(code, start, end):  # 순환신경망(RNN)분석 함수
-    s = time.time()
-    # df = pdr.get_data_yahoo(code, start=start, end=end).reset_index()  # 주식 데이터 크롤링
-    df = fdr.DataReader(symbol=code, start=start, end=end).reset_index()
-    date = df['Date']
-    date = list(date)
-    for i in range(len(date)):
+    s = time.time() # 분석 시작 시간
+    df = fdr.DataReader(symbol=code, start=start, end=end).reset_index() # 데이터 크롤링
+    date = df['Date'] # 날짜 데이터 추출
+    date = list(date) # 리스트 변환
+    for i in range(len(date)): # datetime -> str 변환
         date[i] = date[i].strftime("%Y-%m-%d")
-    date = json.dumps(date)
+    date = json.dumps(date) # json형태로 변환
 
     dataset = df['Close'].values  # 종가 데이터 추출
     dataset = dataset.reshape(dataset.shape[0], 1)  # 1차원배열을 2차원으로 변경
     dataset = dataset.astype('float32')  # int -> float변환
-    scaler = MinMaxScaler(feature_range=(0, 0.9))  # 최대값을 0.9로 설정
+    scaler = MinMaxScaler(feature_range=(0, 1))  # 최대값을 1로 설정
     Dataset = scaler.fit_transform(dataset)
     train_data, test_data = train_test_split(Dataset, test_size=0.2, shuffle=False)  # 8:2로 tarin, test 분리
 
@@ -171,14 +161,14 @@ def rnn(code, start, end):  # 순환신경망(RNN)분석 함수
             x_pred[i][0][-1] = model.predict(
                 x_pred[[i - 1]])  # pred의 이전 row의 1~7번째 데이터를 이용하여 데이터 예측 후, 다음 row의 7번재 인덱스에 저장
 
-    trainPredictPlot = np.zeros(len(dataset) + 7)
-    trainPredictPlot = trainPredictPlot.reshape(trainPredictPlot.shape[0], 1)
-    trainPredictPlot[:, :] = np.nan
-    trainPredictPlot[look_back:len(TrainPredict) + look_back] = TrainPredict
-    trainPredictPlot = trainPredictPlot.reshape(len(trainPredictPlot))
-    trainPredictPlot = np.round(trainPredictPlot, 2)
-    trainPredictPlot = list(trainPredictPlot)
-    trainPredictPlot = json.dumps(trainPredictPlot)
+    trainPredictPlot = np.zeros(len(dataset) + 7) # 시각화를 위한 array 생성
+    trainPredictPlot = trainPredictPlot.reshape(trainPredictPlot.shape[0], 1) # 2차원으로 변환
+    trainPredictPlot[:, :] = np.nan # 모든 값을 결측치로 변경
+    trainPredictPlot[look_back:len(TrainPredict) + look_back] = TrainPredict # 예측값 대입
+    trainPredictPlot = trainPredictPlot.reshape(len(trainPredictPlot)) # 1차원으로 변환
+    trainPredictPlot = np.round(trainPredictPlot, 2) # 소수 둘째 자리만 표시
+    trainPredictPlot = list(trainPredictPlot) # 리스트로 변환
+    trainPredictPlot = json.dumps(trainPredictPlot) # json형태로 변환
 
     testPredictPlot = np.zeros(len(dataset) + 7)
     testPredictPlot = testPredictPlot.reshape(testPredictPlot.shape[0], 1)
@@ -195,34 +185,23 @@ def rnn(code, start, end):  # 순환신경망(RNN)분석 함수
     predPlot = scaler.inverse_transform(predPlot)  # Min-Max변환된 값을 원래대로 되돌림
     predPlot = predPlot.reshape(len(predPlot))
 
+    dataset = dataset.reshape(len(dataset)) # 주식 true값을 1차원으로 변경
+    dataset = list(dataset) # 리스트 변환
 
-
-    dataset = dataset.reshape(len(dataset))
-    dataset = list(dataset)
-
-    # plt.figure(figsize=(20, 10))
-    # plt.plot(dataset, label="true", c='green')
-    # plt.plot(trainPredictPlot, label='train_predict')
-    # plt.plot(testPredictPlot, label='test_predict')
-    # plt.plot(predPlot, label='pred')
-    # plt.legend()
-
-    # plt.show()
-
-    result = np.round(predPlot[-7:].reshape(look_back), 2)
-    predPlot = list(predPlot)
-    predPlot = json.dumps(predPlot)
-    result_d1 = result[0]
+    result = np.round(predPlot[-7:].reshape(look_back), 2) # 일주일 예측 결과값(반올림)
+    predPlot = list(predPlot) # 예측값 리스트 변환
+    predPlot = json.dumps(predPlot) # json형태로 변환
+    result_d1 = result[0] # d+n일 후 예측값
     result_d2 = result[1]
     result_d3 = result[2]
     result_d4 = result[3]
     result_d5 = result[4]
     result_d6 = result[5]
     result_d7 = result[6]
-    result_max = result.max()
-    result_min = result.min()
-    e = time.time()
-    runtime = e - s
+    result_max = result.max() # 예측 최대값
+    result_min = result.min() # 예측 최소값
+    e = time.time() # 분석 종료시간
+    runtime = e - s # 분석에 걸린 시간
     runtime = round(runtime)
     return result, result_d1, result_d2, result_d3, result_d4, result_d5, result_d6, result_d7, result_max, result_min, train_RMSE, test_RMSE, runtime, dataset, trainPredictPlot, testPredictPlot, predPlot, date
 
