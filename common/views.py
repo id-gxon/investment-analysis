4e100248ca2e9a275eca92445a2c13dd4b1840e9
김건우 <wskgw123@gmail.com>
2022-02-11 02:16:35 +0900

    import문 최적화

--- common/views.py
+++ common/views.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .forms import SignupForm
 
 
 @login_required
